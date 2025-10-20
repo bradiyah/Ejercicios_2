683acb4e41a438c380758b8a43d1d9ae6503b729
a25elenapg <htmtf111@gmail.com>
2025-10-20 18:53:38 +0200

    Añadidos metodos

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+
+
+  }
+
+  public static int checkWordLength(String word) {
+    return 0;
+  }
+
+  public static int NumOfVowels(String palabra){
+    return 0;
+
+  }
+
+
+  public static boolean isPrime(int num){
+    return true;
+  }
+
+  public static int checkNumberOfPrimes(int num){
+    return 0;
+  }
+
+  public static int isIdentityMatrix(int num){
+    return 0;
+  }
+
+}
