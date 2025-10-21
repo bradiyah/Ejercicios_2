9a738ad0b6a495072211d4b06a5e4f03bdc5ec32
bradiyah <htmtf111@gmail.com>
2025-10-21 12:01:01 +0200

    Hechos todos menos el ult.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
+
+import static java.awt.SystemColor.text;
+
 public class Main {
+
   public static void main(String[] args) {
+
+      System.out.println(isPrime(2));
+      System.out.println(isPrime(4));
+      System.out.println(isPrime(17));
+      System.out.println(isPrime(20));
 
       String frase = "Hola aqui testeando";
       checkWordLength(frase);
@@ -22,19 +33,51 @@ public class Main {
     }
 
   public static int NumOfVowels(String vocales){
-    return 0;
+      int contador = 0;
+
+      char[] vowels = vocales.toCharArray();
+      for (char c:vowels){
+          if ("aeiouAEIOU".indexOf(c)!= -1){
+              contador++;
+          }
+      }
+    return contador;
 
   }
 
 
-  public static boolean isPrime(int num){
+  public static boolean isPrime(int n){
+      if (n < 2){
+          return false;
+      }
+      if ( n == 2){
+          return true;
+      }
 
-      return false;
+      if (n % 2 == 0){
+          return false;
+      }
+
+      for (int i = 3; i <= Math.sqrt(n) ; i+=2) {
+          if(n % i == 0){
+              return false;
+          }
+
+      }
+
+      return true;
+
+
   }
 
   public static int checkNumberOfPrimes(int [] numbers){
-
-      return 0;
+        int contador = 0;
+        for (int n : numbers){
+            if (isPrime(n)){
+                contador++;
+            }
+        }
+      return contador;
   }
 
   public static boolean isIdentityMatrix(int [][] matrix){
