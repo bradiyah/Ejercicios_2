aa3e821eb0ead7078b369e22a4806810cfbe325f
a25elenapg <htmtf111@gmail.com>
2025-10-21 17:24:21 +0200

    Hechos todos, hoy el ultimo de matrices

--- src/Main.java
+++ src/Main.java
@@ -9,79 +9,91 @@ public class Main {
 
   public static void main(String[] args) {
 
-      System.out.println(isPrime(2));
-      System.out.println(isPrime(4));
-      System.out.println(isPrime(17));
-      System.out.println(isPrime(20));
-
-      String frase = "Hola aqui testeando";
-      checkWordLength(frase);
 
   }
 
-    public static int checkWordLength(String word) {
-        String[] words = word.trim().split("\\s+");
-        int maxLength = 0;
+  public static int checkWordLength(String word) {
+    String[] words = word.trim().split("\\s+");
+    int maxLength = 0;
 
-        for (String palabra : words) {
-            if (palabra.length() > maxLength) {
-                maxLength = palabra.length();
-            }
-        }
-
-        return maxLength;
+    for (String palabra : words) {
+      if (palabra.length() > maxLength) {
+        maxLength = palabra.length();
+      }
     }
 
-  public static int NumOfVowels(String vocales){
-      int contador = 0;
+    return maxLength;
 
-      char[] vowels = vocales.toCharArray();
-      for (char c:vowels){
-          if ("aeiouAEIOU".indexOf(c)!= -1){
-              contador++;
-          }
+
+  }
+
+  public static int NumOfVowels(String vocales) {
+    int contador = 0;
+
+    char[] vowels = vocales.toCharArray();
+    for (char c : vowels) {
+      if ("aeiouAEIOU".indexOf(c) != -1) {
+        contador++;
       }
+    }
     return contador;
 
   }
 
 
-  public static boolean isPrime(int n){
-      if (n < 2){
-          return false;
-      }
-      if ( n == 2){
-          return true;
-      }
-
-      if (n % 2 == 0){
-          return false;
-      }
-
-      for (int i = 3; i <= Math.sqrt(n) ; i+=2) {
-          if(n % i == 0){
-              return false;
-          }
-
-      }
-
+  public static boolean isPrime(int n) {
+    if (n < 2) {
+      return false;
+    }
+    if (n == 2) {
       return true;
+    }
+
+    if (n % 2 == 0) {
+      return false;
+    }
+
+    for (int i = 3; i <= Math.sqrt(n); i += 2) {
+      if (n % i == 0) {
+        return false;
+      }
+
+    }
+
+    return true;
 
 
   }
 
-  public static int checkNumberOfPrimes(int [] numbers){
-        int contador = 0;
-        for (int n : numbers){
-            if (isPrime(n)){
-                contador++;
-            }
+
+  public static int checkNumberOfPrimes(int[] numbers) {
+    int contador = 0;
+    for (int n : numbers) {
+      if (Main.isPrime(n)) {
+        contador++;
+      }
+    }
+    return contador;
+  }
+
+  public static boolean isIdentityMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        if (i == j) {
+          if (matrix[i][j] != 1) {
+            return false;
+          }
+        } else {
+          if (matrix[i][j] != 0) {
+            return false;
+          }
         }
-      return contador;
-  }
+      }
+    }
 
-  public static boolean isIdentityMatrix(int [][] matrix){
-    return false;
-  }
+    return true;
 
-}
+  }
+};
+
+
